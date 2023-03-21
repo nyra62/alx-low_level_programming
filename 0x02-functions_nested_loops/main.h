0a9f5ef4dbc7dbb9f278ac94bb5b26e400a05652
naira <r5n7@outlook.com>
2023-03-22 01:09:49 +0200

    this is how to do it babe

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,7 +16,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void time_teble(void);
+void times_teble(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
